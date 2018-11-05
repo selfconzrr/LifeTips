5893827c8d7bfd74ce52906a2f9b7e1bf0858b87
zhangruirui <zhangruirui@kuaishou.com>
2018-11-05 17:26:31 +0800

    增加退出app提示功能

--- app/src/main/java/com/example/zhangruirui/lifetips/MainActivity.java
+++ app/src/main/java/com/example/zhangruirui/lifetips/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.zhangruirui.lifetips;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @author zhangruirui
+ * email：1138517609@qq.com
+ * GitHub：https://github.com/selfcon
+ * Blog：http://blog.csdn.net/u011489043
+ * Date：11/05/18
+ */
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
   }
+
+  @OnClick(R.id.quit)
+  public void onClick() {
+    doExit();
+  }
+
+  private void doExit() {
+    new AlertDialog.Builder(MainActivity.this)
+        .setTitle("Quit")
+        .setMessage("小主，确认退出吗？")
+        .setIcon(R.drawable.quit)
+        .setPositiveButton("残忍退出",
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog,
+                                  int whichButton) {
+                // TODO: 2018/11/5 这里需要做一些操作：关闭数据库、保存用户设置的屏幕亮度等
+
+                finish();
+              }
+            })
+        .setNegativeButton("再玩一会",
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog,
+                                  int whichButton) {
+                // LEFT DO NOTHING
+              }
+            }).show();
+  }
+
 }
