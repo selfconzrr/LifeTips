b3119138d3c27c1377899b9a4c9caff8a8e665ec
zhangruirui <zhangruirui@kuaishou.com>
2018-11-06 09:56:09 +0800

    增加用户帮助指导,修改应用图标

---
+++ app/src/main/java/com/example/zhangruirui/lifetips/HelpActivity.java
@@ -0,0 +1,24 @@
+package com.example.zhangruirui.lifetips;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * @author zhangruirui
+ * email：1138517609@qq.com
+ * GitHub：https://github.com/selfcon
+ * Blog：http://blog.csdn.net/u011489043
+ * Date：11/06/18
+ * <p>
+ * 用户使用 APP 的帮助指导
+ */
+public class HelpActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.activity_help);
+  }
+}
